cd69aa15fc2fa0c63d522219eb7ceaddb99a795e
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-15 19:45:27 -0300

    Update components

--- src/main.php
+++ src/main.php
@@ -1,21 +1,36 @@
 <?php
 
 use Websyspro\Elements\Shareds\Div;
-use Websyspro\Elements\Shareds\Head;
 
-$head = Head::create()->get();
-
-$div = Div::create()
-  ->attributes([ "id" => "test" ])
-  ->events([ "click" => "alert('test');", "blur" => "alert('test');" ])
-  ->styles([ "color" => "red", "background-color" => "blue" ])
-  ->add( 
-    Div::create()->add(
-      Div::create()->add(
-        "Sub Hello Words"
-      )
-    )
-  )
+echo Div::create()
+  ->styles( [ 
+    "background-color" => "rgb(100,0,0)",
+    "border-radius" => "15px",
+    "padding" => "15px 25px", 
+    "color" => "white",
+  ])
+  ->add( "EMERSON THIAGO" )
   ->get();
 
-print_r( $head );
+// use Websyspro\Elements\Shareds\Div;
+// use Websyspro\Elements\Shareds\Head;
+
+// $head = Head::create()->get();
+
+// $div = Div::create()
+//   ->attributes([ "id" => "test" ])
+//   ->events([ "click" => "alert('test');", "blur" => "alert('test');" ])
+//   ->styles([ "color" => "red", "background-color" => "blue" ])
+//   ->add( 
+//     Div::create()->add(
+//       Div::create()->add(
+//         "Sub Hello Words"
+//       )
+//     )
+//   )
+//   ->get();
+  
+// print_r( "..... ..... .... .... .... ... fdsdafsd fdsfasdfds ..." );
+// print_r( $head );
+
+// print_r( [ "test" => "EMERSON THIAGO SOUSA ALBUQUERQUE"  ]);

105f3b2e701a8af6cb6e2fe7ace44dc55d64f684
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-23 20:11:33 -0300

    Update components

---
+++ src/Shareds/P.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Websyspro\Elements\Shareds;
+
+use Websyspro\Elements\Shareds\Abstract\Dom;
+use Websyspro\Elements\Shareds\Enums\HtmlTag;
+
+class P extends Dom
+{
+  public HtmlTag $htmlTag = HtmlTag::P;
+
+  public static function create(
+  ): Dom {
+    return new static;
+  }   
+}

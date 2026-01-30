dedf0745844b83891109724b84452142ce61924e
e.thiago-pla <e.thiago-pla@platos.parceirosedu.com.br>
2026-01-30 12:36:56 -0300

    Update components

--- src/Shareds/Components/Head.php
+++ src/Shareds/Components/Head.php
@@ -6,24 +6,58 @@ use Websyspro\Elements\Shareds\Abstract\Component;
 use Websyspro\Elements\Shareds\Enums\HtmlTag;
 use Websyspro\Elements\Shareds\Abstract\Dom;
 use Websyspro\Commons\Util;
+use Websyspro\Elements\Shareds\Link;
+use Websyspro\Elements\Shareds\Script;
 
 class Head extends Component
 {
+  public static array $childsStaticsScripts = [];
+  public static array $childsStaticsLinks = [];
+
   public static array $childsStatics = [];
+
   public HtmlTag $htmlTag = HtmlTag::HEAD;
 
   public static function addChildStatic(
     array $childs
   ): void {
-    Head::$childsStatics = array_merge(
-      Head::$childsStatics, $childs
+    Util::mapper(
+      $childs,
+      function( object $child ) {
+        if( $child instanceof Link ){
+          $childExist = Util::inArray( 
+            $child, Head::$childsStaticsLinks
+          );
+
+          if( $childExist === false ){
+            Head::$childsStaticsLinks[] = $child;
+          }
+        } else if( $child instanceof Script ){
+          $childExist = Util::inArray( 
+            $child, Head::$childsStaticsScripts
+          );
+
+          if( $childExist === false ){
+            Head::$childsStaticsScripts[] = $child;
+          }
+        } else {
+          Head::$childsStatics[] = $child;
+        }
+      }
     );
   } 
 
   public function childList(
   ): string {
+    krsort( Head::$childsStaticsLinks );
+    krsort( Head::$childsStaticsScripts );
+
     return $this->childs->merge(
-      Head::$childsStatics
+      array_merge(
+        Head::$childsStatics,
+        Head::$childsStaticsLinks,
+        Head::$childsStaticsScripts
+      )
     )->mapper( fn( Dom|string $child ) => (
       Util::isString( $child ) 
         ? $child : $child->get()

238405f77b745189f411aa1b199b1838111c3fed
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-12 20:55:14 -0300

    Release 1.0.33

--- src/Doms/Body.php
+++ src/Doms/Body.php
@@ -9,4 +9,12 @@ class Body
 extends Dom
 {
   protected DomType $domType = DomType::BODY;
+
+  public function __construct(
+    private array $childs = []
+  ){
+    parent::__construct(
+      $childs, [], [ "margin" => "0px" ]
+    );
+  }   
 }

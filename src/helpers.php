93a54a117fde1e7094daa81bc0017ffb540c522e
websyspro <cpd.emersontsa@gmail.com>
2026-09-13 11:29:46 -0300

    Release 1.0.50

--- src/helpers.php
+++ src/helpers.php
@@ -9,7 +9,7 @@ use Websyspro\Elements\Doms\Html;
 use Websyspro\Elements\Doms\Icons\Icon3p;
 use Websyspro\Elements\Doms\StyleLink;
 use Websyspro\Elements\Doms\Meta;
-use Websyspro\Elements\Enums\IconWeight;
+use Websyspro\Elements\Enums\IconTypeWeight;
 
 function App(
   mixed ...$childs
@@ -61,7 +61,7 @@ function Div(
 function Icon3p(
   int $size,
   array $fill = [ 0,0,0 ],
-  IconWeight $iconSize = IconWeight::iw300
+  IconTypeWeight $iconTypeWeight = IconWeight::iw300
 ): Icon3p {
-  return new Icon3p($size, $fill, $iconSize);
+  return new Icon3p($size, $fill, $iconTypeWeight);
 }
